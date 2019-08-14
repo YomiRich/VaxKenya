79f174cda763c733d39cdc2d433269a34b2c360a
magechaP <magechapeter@gmail.com>
2019-08-14 12:03:58 +0300

    create various user tests for our user and his/her details

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,6 +1,17 @@
 import unittest
-# from app.models import User
+from app.models import User
 
 class UserTest(unittest.TestCase):
+    
     def setUp(self):
-        self.new_user
+        self.new_user = User(password = '1234')
+    
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password_hash is not None)
+
+    def tes_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('1234'))            
